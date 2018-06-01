672b3380c65dbad0b84b001e10d6e54dd4decf44
kirkaiya-at-544123295116 <kirkdavi@amazon.com>
2018-06-01 12:29:06 -0700

    Adding initial project files copied from the pre-git folder.

---
+++ Auth/CognitoGroupAuthorizationHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CognitoGroupAuthorizer
+{
+    class CognitoGroupAuthorizationHandler : AuthorizationHandler<CognitoGroupAuthorizationRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CognitoGroupAuthorizationRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == "cognito:groups"))
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            var group = context.User.FindFirst(c => c.Type == "cognito:groups").Value;
+
+            if (group == requirement.CognitoGroup)
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
