f953267c1fb03fec6d30e081fb329a0fad8278e2
kirkaiya-at-544123295116 <kirkdavi@amazon.com>
2018-11-08 12:35:24 -0800

    removed cookie domain to test

--- Startup.cs
+++ Startup.cs
@@ -54,7 +54,6 @@ namespace CartService
             services.AddSession(o => {
                 o.IdleTimeout = TimeSpan.FromMinutes(30);
                 o.Cookie.HttpOnly = true;
-                o.Cookie.Domain = ".awstechsummit.com";
             });
 
             services.AddAuthentication(options => options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme)
